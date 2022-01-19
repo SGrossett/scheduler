aa1c86e8414d52c7561620036ddf977b90aa3235
SGrossett <selene_816@hotmail.com>
2022-01-19 16:09:31 -0500

    Create a Header component that accepts time as a prop and an Empty component that accepts onAdd as a prop. Create the stores to test out the Header and Empty props.

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index.js"
 import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
 
 storiesOf("Button", module)
   .addParameters({
@@ -149,9 +150,10 @@ storiesOf("DayListItem", module)
     .add("Appointment with Time", () => <Appointment time="12pm"/>);
   
   storiesOf("Appointment", module)
-  .addParameters({
-    backgrounds: [{ name: "white", value: "#fff", default: true }]
-  })
-  .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />);
