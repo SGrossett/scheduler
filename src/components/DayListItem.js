737337b54d5ee14e862c21b489796b7cd3bd55c9
SGrossett <selene_816@hotmail.com>
2022-01-19 01:11:01 -0500

    Add test cases to check if spots is being dsiplayed correctly and write a function called formatSpots in the DayListItem component that will format the props.spots to make our tests pass.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,20 @@ import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
   const { name, spots, setDay, selected } = props;
-  const dayClass = classNames('day-list__item', {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0
   });
 
+  const formatSpots = () => {
+    if (spots === 0) return "no spots remaining";
+    if (spots > 0) return `${spots}${spots === 1 ? " spot" : " spots"} remaining`;
+  }
+
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text-regular">{name}</h2>
-      <h3 className="text-light">{spots} spots remaining</h3>
+      <h3 className="text-light">{formatSpots()}</h3>
     </li>
   );
 }
