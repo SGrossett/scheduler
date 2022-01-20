2d5d1e3c65a3e785cba04d5fdde87982880dfc6c
SGrossett <selene_816@hotmail.com>
2022-01-19 21:04:01 -0500

    Create a form component that imports InterviewerList and Button component. Add the stories for Create and Edit and chain them to previous Appoint stories.

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const { interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          interviewers={interviewers}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={() => onCancel()}>Cancel</Button>
+          <Button confirm onClick={() => onSave()}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -8,9 +8,16 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
-import Appointment from "components/Appointment/index.js"
-import Header from "components/Appointment/Header"
-import Empty from "components/Appointment/Empty"
+import InterviewerList from "components/InterviewerList";
+import InterviewerListItem from "components/InterviewerListItem";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,7 +145,7 @@ storiesOf("DayListItem", module)
     .add("Clickable", () => (
       <InterviewerList
         interviewers={interviewers}
-        onChange={action("setInterviewer")}
+        setInterviewer={action("setCurrentInterviewerID")}
       />
     ));
 
@@ -156,4 +163,37 @@ storiesOf("DayListItem", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm" />)
-    .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => 
+      <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action("onEdit")} 
+        onDelete={action("onDelete")} 
+      />)
+    .add("Confirm", () => 
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")} 
+      onCancel={action("onCancel")} 
+    />)
+    .add("Status", () => <Status message="Deleting" />)
+    .add("Error", () => 
+      <Error 
+        message="Could not delete appointment" 
+        onClose={action("onClose")}
+    />)
+    .add("Create", () => 
+      <Form
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+    />)
+    .add("Edit", () => 
+      <Form 
+        student="Selene Grossett"
+        interviewer={2}
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+    />);
