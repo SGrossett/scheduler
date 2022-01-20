24fb7d626c6227538d52bb0609ccaed5726fff9b
SGrossett <selene_816@hotmail.com>
2022-01-19 23:10:21 -0500

    Update the Appointment component so that all components will render a Header that take in a time prop and so that the Appointment will render the Shows component if props.interview is truthy or Empty if not.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 import "../Appointment/styles.scss"
 
 export default function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
 
   const formatAppointments = () => {
     return time ? `Appointment at ${time}` : `No Appointments`;
   }
   return (
-    <article className="appointment">{formatAppointments()}</article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
