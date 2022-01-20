15126cfa71108d443921a6d3604dd3046514df18
SGrossett <selene_816@hotmail.com>
2022-01-19 23:14:49 -0500

    Add a reset() function to the Form component that sets student and interviewer to an empty string. Add a cancel function to the Form component that calls reset() and props.onCancel. Add event handler to onSubmi that prevents default behaviour.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,10 +7,20 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    student("");
+    interviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -28,7 +38,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={() => onCancel()}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={() => onSave()}>Save</Button>
         </section>
       </section>
