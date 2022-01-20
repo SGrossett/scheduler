c97534f02e44cda7ea9d3b2b1b29268fa5d42e45
SGrossett <selene_816@hotmail.com>
2022-01-20 12:58:19 -0500

    Update component to show correctly.

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,12 +10,12 @@ export default function InterviewerListItem(props) {
 
   return (
     <li className={interviewerClass} onClick={setInterviewer}>
-    <img
-      className="interviewers__item-image"
-      src={avatar}
-      alt={name}
-    />
-    { selected && name}
-  </li>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      { selected && name}
+    </li>
   );
 }
