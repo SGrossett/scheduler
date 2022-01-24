0a87242a0baabc43c729de4d003a018839fe58f4
SGrossett <selene_816@hotmail.com>
2022-01-24 14:13:16 -0500

    Create and export a useVisualMode function that utilizes the custom Hooks. Create tests to make sure the function is working correctly.

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setHistory([...history.slice(0, history.length - 1), newMode])
+    } else {
+      history.push(newMode);
+    }
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length -1 ]);
+    }
+  }
+
+  return { mode, transition, back };
+}
