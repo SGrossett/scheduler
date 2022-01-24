6f012f6bb3a5f42ed4650db52a3ef66e48c04daa
SGrossett <selene_816@hotmail.com>
2022-01-24 11:49:14 -0500

    Implement the getInterviewersForDay function and create tests to make sure the function is working correctly.

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,8 @@
 export function getAppointmentsForDay(state, day) {
   const { days, appointments } = state;
   const filteredAppointments = days.find((item) => item.name === day);
-
-  if (days.length < 1 || filteredAppointments === undefined) {
-    return [];
-  }
+  
+  if (days.length < 1 || filteredAppointments === undefined) return [];
 
   const results = filteredAppointments.appointments.map((id) => appointments[id]);
   return results;
@@ -19,4 +17,14 @@ export function getInterview(state, interview) {
   }
 
   return null;
+};
+
+export function getInterviewersForDay(state, day) {
+  const { days, interviewers } = state;
+  const filteredInterviewers = days.find((item) => item.name === day);
+
+  if (days.length < 1 || filteredInterviewers === undefined) return [];
+
+  const results = filteredInterviewers.interviewers.map((id) => interviewers[id]);
+  return results;
 };
