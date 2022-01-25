aa16d08661e174bd6a089e0fa2941c9ebfe4fb19
SGrossett <selene_816@hotmail.com>
2022-01-24 23:47:02 -0500

    Create a function called save and call the props.bookInterview function with the appointment id and interview as arguments from within the function. Transition to the SHOW mode after calling props.bookInterview.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,34 +3,36 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 import "../Appointment/styles.scss"
 
 export default function Appointment(props) {
-  const { time, interview } = props;
-
+  const { time, interview, id, bookInterview, interviewers } = props;
+  
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
 
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+
+    bookInterview(id, interview)
+    transition(SHOW);
+    
+  }
+
   return (
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && (
-        <Show
-          student={interview.student}
-          interviewer={interview.interviewer}
-        />
-      )}
-      {mode === CREATE && (
-        <Form
-          interviewers={[]}
-          onCancel={() => back()}
-        />
-      )}
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} />}
+      {mode === CREATE && <Form interviewers={interviewers} onCancel={() => back()} onSave={save} />}
     </article>
   );
 }
