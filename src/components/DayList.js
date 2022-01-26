c64a04f4ea2c05ab2c97eb064334afa783de6a6a
SGrossett <selene_816@hotmail.com>
2022-01-25 19:54:48 -0500

    Update the DayList function so that you can select the day and see the different appointments.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const { value, onChange } = props;
+  const { value, setDay } = props;
   const dayList = props.days.map((day) => (
     <DayListItem 
       key={day.id}
       name={day.name} 
       spots={day.spots} 
       selected={day.name === value}
-      setDay={onChange}  
+      setDay={setDay}  
     />
   ));
   return dayList;
