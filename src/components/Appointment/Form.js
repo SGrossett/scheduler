0aa88aaa832a965a8b56b74927d77a34e5276115
SGrossett <selene_816@hotmail.com>
2022-01-27 01:03:21 -0500

    Create a function called validate to prevent submit unless there is a student and an interviewer is selected. Add an error state and a section in the HTML to display the error.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
   const { interviewers, onSave, onCancel } = props;
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("");
@@ -17,7 +18,17 @@ export default function Form(props) {
     onCancel();
   }
 
-  const save = () => {
+  const validate = () => {
+    if (!student) {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (student && !interviewer) {
+      setError("Please select an interviewer");
+      return;
+    }
+
+    setError("null");
     onSave(student, interviewer);
   }
   
@@ -35,6 +46,7 @@ export default function Form(props) {
             onChange={(event) => setStudent(event.target.value)}
             data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           value={interviewer}
@@ -45,7 +57,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
